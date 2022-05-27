a95072efb5448a28f9973df10ebef9130dcce8b8
shely25 <shely.ece.25@gmail.com>
2022-05-27 16:44:55 +0600

    dashboard connected

--- src/Components/Homepage/Banner.js
+++ src/Components/Homepage/Banner.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import banner from '../../Images/banner.jpg'
 import './Banner.css'
 
 const Banner = () => {
     return (
-        <div className=''>
-            <h1 className='flex justify-center items-center text-4xl banner text-[#FFFFFF] font-semibold'>Welcome to our site.</h1>
+        <div className='flex justify-center items-center banner'>
+            <div>
+                <h1 className='text-4xl font-semibold'>Welcome to our site.</h1>
+                <p className='text-2xl'>Here You can find your necessary electric tools for your Shop at a pikary rate.</p>
+            </div>
 
         </div>
     );
---
+++ src/fakedata.js
@@ -0,0 +1,65 @@
+const data = [
+    {
+        Name: "Electric Sockets",
+        Image: "https://i.ibb.co/LP0zhLj/sockets.jpg",
+        Description: "The brand New Premium Sockets.",
+        MinimumQuanrity: 200,
+        Available: 5000,
+        price: 300
+
+    },
+    {
+        Name: "Electric Tester",
+        Image: "https://i.ibb.co/Qk5SDrX/tester.jpg",
+        Description: "The brand tester.there are many type of tester.You can chosse your own one.",
+        MinimumQuanrity: 300,
+        Available: 5000,
+        price: 100
+
+    },
+    {
+        Name: "Electric Switch",
+        Image: "https://i.ibb.co/6brrz71/Switch.jpg",
+        Description: "The brand New Premium Switch.",
+        MinimumQuanrity: 500,
+        Available: 5500,
+        price: 350
+
+    },
+    {
+        Name: "Electric Drill Machine",
+        Image: "https://i.ibb.co/D7RWxTs/Drill.jpg",
+        Description: "The brand New Premium drill machine.which cn take upto 230 to 240V",
+        MinimumQuanrity: 1000,
+        Available: 10500,
+        price: 2500
+
+    },
+    {
+        Name: "Electric Sockets",
+        Image: "https://i.ibb.co/LP0zhLj/sockets.jpg",
+        Description: "The brand New Premium Sockets.",
+        MinimumQuanrity: 1000,
+        Available: 5000,
+        price: 300
+
+    },
+    {
+        Name: "Electric Wire",
+        Image: "https://i.ibb.co/Km5TvkK/wire.jpg",
+        Description: "The most necessary Wire.4 type of wire are available",
+        MinimumQuanrity: 100,
+        Available: 5400,
+        price: 200
+
+    },
+    {
+        Name: "Electric Multimeter",
+        Image: "https://i.ibb.co/tH9LXw4/multimeter.jpg",
+        Description: "The brand New Premium Multimeter.",
+        MinimumQuanrity: 100,
+        Available: 5000,
+        price: 1500
+
+    },
+]
